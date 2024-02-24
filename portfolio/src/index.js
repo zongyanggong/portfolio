5ff6a8011f9a9b2e1978205fadb7ee49fb7e9d14
DESKTOP-44T2J6J\simon <zongyanggong@gmail.com>
2024-02-24 03:56:51 -0500

    test demo

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
+import { hydrate, render } from "react-dom";
 // import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
-
-import { hydrate, render } from "react-dom";
 
 const APP = (
   <Provider store={store}>
